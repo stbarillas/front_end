e24c02eadbcace8cfaf0fee5c03bb2fff9a3084f
stbarillas <stbarillas@Steves-MacBook-Pro.local>
2019-06-24 02:07:18 -0700

    added instrument card that displays info from api call

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import InstrumentCard from './components/cards'
+import Grid from '@material-ui/core/Grid';
+
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            instruments: [],
+        }
     }
+
     componentDidMount() {
         this.apiCall()
     }
+
   // Fetches API data
     apiCall() {
     // API call function
-        fetch('http://127.0.0.1:8000/users/?format=json')
+        fetch('http://127.0.0.1:8000/instruments/?format=json')
         // Converts API response to json
             .then((response) => {
               return response.json();
@@ -20,6 +27,7 @@ class App extends React.Component {
             // Extracts and saves data to app state under 'students'
             .then((data) => {
               console.log(data);
+              this.updateInstruments(data)
             })
             // Displays error if API call is unsuccessful
             .catch((err) => {
@@ -28,25 +36,24 @@ class App extends React.Component {
             })
     }
 
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-    );
+  // Updates Instrument State
+    updateInstruments(data) {
+        const instruments = data;
+        this.setState({instruments: instruments})
+    }
+
+    render() {
+        return (
+            <Grid container spacing={3}>
+                {this.state.instruments.map((instrument)=>{
+                    return (
+                            <Grid item>
+                                <InstrumentCard data={instrument}/>
+                            </Grid>
+                    )
+                })}
+            </Grid>
+        );
   }
 }
 
