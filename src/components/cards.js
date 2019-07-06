0e4616827e64d225f12f18dcf6272e2a6c1c4fd8
stbarillas <stbarillas@Steves-MacBook-Pro.local>
2019-07-06 03:13:13 -0700

    moved queue functions to tasks file

--- src/components/cards.js
+++ src/components/cards.js
@@ -7,7 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import LeaveQueue from './queue'
+import {joinQueue, leaveQueue} from '../tasks/queue'
 
 const useStyles = makeStyles({
     card: {
@@ -36,98 +36,15 @@ function InstrumentCard(props) {
                 />
             </CardActionArea>
             <CardActions>
-                <Button size="small" color="primary" onClick={()=>joinWaitList(props)}>
+                <Button size="small" color="primary" onClick={()=>joinQueue(props)}>
                     Join Waitlist
                 </Button>
-                <Button size="small" color="primary" onClick={()=>leaveWaitList(props)}>
+                <Button size="small" color="primary" onClick={()=>leaveQueue(props)}>
                     Leave Waitlist
                 </Button>
             </CardActions>
         </Card>
     );
-}
-
-function joinWaitList(props) {
-    const url = 'http://127.0.0.1:8000/checklist/',
-        isoDate = new Date().toISOString(),
-        user_id = sessionStorage.getItem('user_id'),
-        full_name = sessionStorage.getItem('full_name');
-    console.log(user_id)
-    const data = {
-        "user": 'http://127.0.0.1:8000/users/' + user_id + '/',
-        "user_pk": user_id,
-        "display_name": full_name,
-        "instrument_pk": props.data.id,
-        "created_date": isoDate,
-        "ownership_date": isoDate,
-    }
-    fetch(url, {
-        method: 'POST', // or 'PUT'
-        body: JSON.stringify(data), // data can be `string` or {object}!
-        headers:{
-            'Content-Type': 'application/json',
-            'Authorization': 'Token ' + sessionStorage.getItem('token')
-        }
-    })
-        .then(res => res.json())
-        .then(response => console.log('Success:', JSON.stringify(response)))
-        .catch(error => console.error('Error Posting Checklist:', error));
-
-}
-
-function leaveWaitList(props) {
-    const url = 'http://127.0.0.1:8000/checklist/';
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Token ' + sessionStorage.getItem('token')
-        }
-    })
-    // If response is ok, translate to json. Otherwise, throw Error
-        .then(response => {
-            if(response.ok){
-                return response.json()
-            } else {
-                throw new Error(response.status);
-            }
-        })
-        .then(response => {
-            let filteredChecklist = response.filter(
-                (listEntry)=> {
-                    const userId = String(listEntry.user_pk),
-                        instrumentId = String(listEntry.instrument_pk);
-                    // console.log(instrumentId)
-                    // Only returns checklist entries that match user pk and instrument pk
-                    return userId.indexOf(sessionStorage.getItem('user_id')) !== -1 &&
-                        instrumentId.indexOf(props.data.id) !== -1;
-                })
-            console.log(filteredChecklist)
-            return filteredChecklist
-        })
-        .then(entry => {
-            const id = entry[0].id,
-                url = 'http://127.0.0.1:8000/checklist/' + id + '/';
-            fetch(url, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Token ' + sessionStorage.getItem('token')
-                }
-            })
-            // If response is ok, translate to json. Otherwise, throw Error
-                .then(response => {
-                    if (response.ok) {
-                        return console.log('Success:', JSON.stringify(response));
-                    } else {
-                        throw new Error(response.status);
-                    }
-                })
-                .catch(error => console.error('API error:', error));
-
-        })
-        .catch(error => console.error('API error:', error));
-
 }
 
 export default InstrumentCard;
